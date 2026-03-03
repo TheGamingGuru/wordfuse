3fb738db3d94341cd52c54fec73cbc06f8e2b45b
TheGamingGuru <mattzahballs@hotmail.com>
2026-03-03 18:23:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Analytics from './Analytics.jsx'
 import './index.css'
+
+const isAnalytics = new URLSearchParams(window.location.search).get('admin') !== null;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAnalytics ? <Analytics /> : <App />}
   </React.StrictMode>,
 )
